5e3e88b4c4b7ebeaca3c7400e1e5383482daaf6f
Soumik Dutta <shalearkane@gmail.com>
2023-05-21 00:37:11 +0530

    finish personal projects
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- section/personal_projects.go
+++ section/personal_projects.go
@@ -1,8 +1,27 @@
 package section
 
+import (
+	"bytes"
+	"text/template"
+)
+
 type PersonalProject struct {
 	ProjectName string            `json:"projectName"`
 	TimePeriod  TimePeriod        `json:"timePeriod"`
 	Links       []Link            `json:"links"`
 	Details     []MarkdownSnippet `json:"details"`
+}
+
+var tplPersonalProjects = template.Must(template.ParseFiles("section/tex/personal_projects.tex"))
+
+func PreparePersonalProjectsSection(personalProjects []PersonalProject) (string, error) {
+	var buf bytes.Buffer
+	err := tplPersonalProjects.Execute(&buf, personalProjects)
+	if err != nil {
+		println(err.Error())
+		return "", err
+	}
+
+	println(buf.String())
+	return buf.String(), nil
 }
