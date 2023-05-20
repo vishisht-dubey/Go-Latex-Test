f8132661c0d0c4e9b0352effe2c8ce0aea1d9aa4
Soumik Dutta <shalearkane@gmail.com>
2023-05-20 12:15:52 +0530

    fix timeperiod
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- section/research_experience.go
+++ section/research_experience.go
@@ -1,12 +1,10 @@
 package section
 
 type ResearchExperience struct {
-	ProjectTitle   string   `json:"projectTitle"`
-	Institute      string   `json:"institute"`
-	Location       string   `json:"location"`
-	ProjectAdvisor string   `json:"projectAdvisor"`
-	StartDate      string   `json:"startDate"`
-	EndDate        string   `json:"endDate"`
-	IsOngoing      string   `json:"isOngoing"`
-	Details        []string `json:"details"`
+	ProjectTitle   string     `json:"projectTitle"`
+	Institute      string     `json:"institute"`
+	Location       string     `json:"location"`
+	ProjectAdvisor string     `json:"projectAdvisor"`
+	TimePeriod     TimePeriod `json:"timePeriod"`
+	Details        []string   `json:"details"`
 }
