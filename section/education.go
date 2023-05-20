c00c57933d52fc9fa033bdf8dcc9c99cf8c44bc1
Soumik Dutta <shalearkane@gmail.com>
2023-05-20 10:04:04 +0530

    update demo
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- section/education.go
+++ section/education.go
@@ -9,13 +9,16 @@ import (
 	"time"
 )
 
-type Date time.Time
+type Date struct {
+	raw_time    time.Time
+	String_time string
+}
 
 type Education struct {
 	StartDate   Date   `json:"start_date"`
 	EndDate     Date   `json:"end_date"`
 	Institution string `json:"institution"`
-	Location    string `json:"Location"`
+	Location    string `json:"location"`
 	Course      string `json:"course"`
 	Degree      string `json:"degree"`
 }
@@ -25,21 +28,26 @@ func (j *Date) UnmarshalJSON(b []byte) error {
 	s := strings.Trim(string(b), "\"")
 	t, err := time.Parse("2006-01-02", s)
 	if err != nil {
+		println(err)
 		return err
 	}
-	*j = Date(t)
+	str := t.Format("%Y-%M-%D")
+	*j = Date{
+		t,
+		str,
+	}
 	return nil
 }
 
 func (j Date) MarshalJSON() ([]byte, error) {
-	return json.Marshal(time.Time(j))
+	return json.Marshal(time.Time(j.raw_time))
 }
 
 var tpl = template.Must(template.ParseFiles("section/tex/education.tex"))
 
 func PrepareEducationSection(e Education) (string, error) {
 	var buf bytes.Buffer
-	err := tpl.Execute(&buf, nil)
+	err := tpl.Execute(&buf, e)
 	if err != nil {
 		// Handle the error if any and return
 		fmt.Println(err.Error())
